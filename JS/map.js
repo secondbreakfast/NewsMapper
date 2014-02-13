367d99ec61dc0b3074d28470f418075c68aa2ffd
Zhila <zfemadi@gmail.com>
2014-02-12 20:32:07 -0800

    Added jquery easing, modified styles

--- JS/map.js
+++ JS/map.js
@@ -1,9 +1,11 @@
-//Would like it to pull more API's and data if I have time. Right now, not many valid data points appear. 
+//Would like it to pull more API's and add filtering w/localstorage if I have time. Right now, not many valid data points appear. 
 
 $(document).ready(function () {
     init();
 });
 
+
+//Declaring lots of global variables because of Google Maps and its rigorous use of certain functions for mapping.
 
 var i = 0;
 var gmapData = {};
@@ -14,6 +16,8 @@ var mapElement;
 var map;
 var infowindow = new google.maps.InfoWindow();
 
+
+//Starts the map, with lots of map styling. 
 function init() {
 
     var mapOptions = {
@@ -132,33 +136,60 @@ function init() {
 pullNews();
 
 }
+
+//API call to the New York Times.
 function pullNews() {
     $.ajax({
         url: 'http://jsonp.jit.su/?url=http://api.nytimes.com/svc/news/v3/content/all/world/.json?api-key=ab2dad7cc399b957a35c3c2e0218e756%3A14%3A68754846',
         datatype: 'json',
         success: function (newsData) {
+            //console.log(newsData);
+/*           
+    DEFINITIONS:
+    newsData.results : Each news item returned. 
+    newsData.results.geo_facet : Geographic tag included w/API. 
+    newsData.results.country : New key-value that stores cleaned up country data. 
 
+    Issue: Data could not be used as-is. Inconsistent tags and formats produced errors. 
+    Resolution: If AJAX call sucessful, a new key-value (country) is added to result.  
+
+    NOTES: 
+    1) Attribute called Geo_Facet often had multiple values. Index 0 was chosen for simplicity in processing. 
+    2) Geo_Facet tag is not formatted consistently. Sometimes refers to city, country, landmark, or combination of all.  
+
+    */ 
             for (var i = 0; i < newsData.results.length; i++) {
-                try {
+                                try {
+                    //If no geo tags, set country to null.
                     if (!newsData.results[i].geo_facet[0]) {
-                        newsData.results[i].country = "None";
+                        newsData.results[i].country = null;
+                    //Some country info is in parentheses. Used regex to pull country name from tag. 
                     } else if (newsData.results[i].geo_facet[0].match(/\((.*)\)/)) {
                         newsData.results[i].country = newsData.results[i].geo_facet[0].match(/\((.*)\)/);
                         newsData.results[i].country = newsData.results[i].country[1];
-
+                    //Someimes it's all good. If not empty or not with parentheses, just add the tag to country. 
                     } else {
                         newsData.results[i].country = newsData.results[i].geo_facet[0];
                     }
+                    //If error, set country to null.
                 } catch (e) {
-                    newsData.results[i].country = "None";
+                    newsData.results[i].country = null;
                 }
             }
+            //Sets news data to global variable. Not the best thing to do, but I had to get it to work. 
             gmapData = newsData;
+
+            //Run next function, which is a workaround to asynchronous Goog functions. 
            forceSync(gmapData);
 
        }
    });
 }
+
+/*
+This loop passes each result into the geocode function individually, which allows for custom infowidows. Each item at index i is passed into a temporary variable for geocoding.  
+
+*/
 
 function forceSync(gmapData){
 
@@ -169,6 +200,10 @@ function forceSync(gmapData){
 }
 }
 
+/*
+The bane of my existence for the past week. 
+*/
+
 function geocode(i, dataTemp) {
 
    var geocoder = new google.maps.Geocoder();
@@ -177,12 +212,12 @@ function geocode(i, dataTemp) {
 }, function (results, status) {
    if (status == google.maps.GeocoderStatus.OK) {
     gmapData.results[i].loc = results[0].geometry.location;
-    gmapData.results[i].lat = results[0].geometry.location.lat();
-    gmapData.results[i].lon = results[0].geometry.location.lng();
+    //gmapData.results[i].lat = results[0].geometry.location.lat();
+    //gmapData.results[i].lon = results[0].geometry.location.lng();
     var marker = new google.maps.Marker({
                 position: (results[0].geometry.location),
                 map: map,
-                icon: "C:/Users/zhilabug/Documents/Rocketu/local.rocketu.com/NewsMapper/img/marker_icon.jpg"
+                icon: "C:/Users/zhilabug/Documents/Rocketu/local.rocketu.com/NewsMapper/img/marker_icon.png"
 });
 google.maps.event.addListener(marker,
                 "click", function () {
